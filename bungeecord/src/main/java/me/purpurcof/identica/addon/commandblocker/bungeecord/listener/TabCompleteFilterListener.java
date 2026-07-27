a75bc197be0912b9b20859061e01add4bc35a67a
purpurcof <purpurcof@gmail.com>
2026-07-27 20:30:48 +0500

    fix: filter backend tab suggestions via TabCompleteResponseEvent

--- bungeecord/src/main/java/me/purpurcof/identica/addon/commandblocker/bungeecord/listener/TabCompleteFilterListener.java
+++ bungeecord/src/main/java/me/purpurcof/identica/addon/commandblocker/bungeecord/listener/TabCompleteFilterListener.java
@@ -6,6 +6,7 @@ import me.purpurcof.identica.addon.commandblocker.service.CommandFilterService;
 import me.purpurcof.identica.addon.commandblocker.util.AliasNormalizer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.TabCompleteEvent;
+import net.md_5.bungee.api.event.TabCompleteResponseEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
@@ -21,6 +22,30 @@ public class TabCompleteFilterListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onEvent(@NotNull TabCompleteEvent event) {
+        if (!(event.getSender() instanceof ProxiedPlayer player))
+            return;
+
+        if (!commandFilterService.isBlocked(player.getUniqueId()))
+            return;
+
+        Set<String> allowedNames = definitionCollector.getAllowedDuringAuthCommandNames();
+
+        event.getSuggestions().removeIf(suggestion -> {
+            if (suggestion == null || suggestion.isBlank())
+                return true;
+
+            String firstWord = AliasNormalizer.normalize(suggestion);
+            int spaceIndex = firstWord.indexOf(' ');
+            if (spaceIndex > 0) firstWord = firstWord.substring(0, spaceIndex);
+            return !allowedNames.contains(firstWord);
+        });
+
+        if (event.getSuggestions().isEmpty())
+            event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onResponse(@NotNull TabCompleteResponseEvent event) {
         if (!(event.getSender() instanceof ProxiedPlayer player))
             return;
 
